b68ea50173f2d734034cc625dfa1e2423cc4e514
Anagha <anaghalakshmeeka@gmail.com>
2024-01-28 22:24:59 +0530

    testing versel app with server on loacl host

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import './App.css'
 import Join from './pages/Join'
 import Home from './pages/Home'
 import io from 'socket.io-client';
-// const socket = io.connect('https://chat-app-server-2-ekv0.onrender.com'); // Replace with your server URL
-const socket = io.connect('https://chat-app-cf726.web.app'); // Replace with your server URL
-
-
+// const socket = io.connect('https://chat-app-server-2-ekv0.onrender.com'); // render
+// const socket = io.connect('https://chat-app-cf726.web.app'); // firebase
+const socket = io.connect('http://localhost:3001'); // Replace with your server URL
 
 function App() {
 
